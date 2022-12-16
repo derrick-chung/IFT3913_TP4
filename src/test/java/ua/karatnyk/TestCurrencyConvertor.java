3cd8b843e3a4900efba5cf6d8ed0330ec22baf28
derrick-chung <derrickch007@gmail.com>
2022-12-16 13:06:57 -0500

    Added tests
    added a few blackbox tests

--- src/test/java/ua/karatnyk/TestCurrencyConvertor.java
+++ src/test/java/ua/karatnyk/TestCurrencyConvertor.java
@@ -1,4 +1,25 @@
 package ua.karatnyk;
 
+import ua.karatnyk.impl.*;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public class TestCurrencyConvertor {
+    OfflineJsonWorker manager = new OfflineJsonWorker();
+    CurrencyConversion conversion = manager.parser();
+    CurrencyConvertor convertor;
+
+    public void BlackBoxTest() {
+        try {
+            convertor.convert(0, "USD", "CAD", conversion);
+            convertor.convert(10000, "USD", "CAD", conversion);
+            convertor.convert(0, "CAD", "USD", conversion);
+            convertor.convert(10000, "USD", "CAD", conversion);
+        } catch (ParseException PE) {
+            PE.printStackTrace();
+        }
+    }
 }
